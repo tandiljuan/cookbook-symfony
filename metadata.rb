cd3b594866987163c61f56fde1a3d0906a91cbcf
Juan Manuel Lopez <git.nrbye@slmail.me>
2013-10-04 22:48:18 -0300

    Add cookbook files to the repo

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             "cookbook-core"
+maintainer       "Juan Manuel Lopez"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Bootstrap a vagrant environment"
+version          "0.0.1"
+
+depends 'apt', '= 1.8.0'
+depends 'vim', '= 1.0.2'
+depends 'smbfs', '= 0.2.0'
