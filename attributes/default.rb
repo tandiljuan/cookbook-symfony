8ec6ddd6adaa4b01076511f2b4e3279529c9abab
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-06-01 09:18:31 -0300

    Define NFS workspace path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 # Vagrant Settings
 default[:vagrant][:workspace_path] = '/opt/vboxsf/workspace'
+default[:vagrant][:workspace_nfs_path] = '/opt/nfs/workspace'
 
 # Core settings
 default[:core][:workspace_path] = '/opt/workspace'
