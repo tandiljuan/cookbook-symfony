5a68dcc88591e4713e2ce65ddb2dbcf1f256761a
Juan Manuel Lopez <git.nrbye@slmail.me>
2014-06-01 09:40:10 -0300

    Use NFS when it's available

--- recipes/init.rb
+++ recipes/init.rb
@@ -29,6 +29,22 @@ link node[:core][:workspace_path] do
   only_if { samba_is_mounted and not File.exist? node[:core][:workspace_path] }
 end
 
+# If there is no workspace, set the nfs shared directory as workspace
+nfs_workspace_exist = false
+ruby_block "check_nfs_workspace_created" do
+  block do
+    if File.directory? node[:vagrant][:workspace_nfs_path]
+      nfs_workspace_exist = true
+    end
+  end
+  action :create
+end
+
+link node[:core][:workspace_path] do
+  to node[:vagrant][:workspace_nfs_path]
+  only_if { nfs_workspace_exist and not File.exist? node[:core][:workspace_path] }
+end
+
 # If there is no workspace, set the vagrant shared directory as workspace
 vagrant_workspace_exist = false
 ruby_block "check_vagrant_workspace_created" do
