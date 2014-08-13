584c5b1957a7fd416ce3dd9c66f36984a6ca586e
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-08-13 00:03:50 -0300

    Sort and indent cookbooks dependencies

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "Apache 2.0"
 description      "Bootstrap a vagrant environment"
 version          "0.0.1"
 
-depends 'apt', '= 1.8.0'
-depends 'vim', '= 1.0.2'
-depends 'smbfs', '= 0.2.0'
+depends "apt",              "= 1.8.0"
+depends "smbfs",            "= 0.2.0"
+depends "vim",              "= 1.0.2"
