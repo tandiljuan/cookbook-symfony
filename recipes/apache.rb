0f7cb7b3bb0b07f6214d0ebdbd46ca411405b013
Juan Manuel Lopez <git.nrbye@slmail.me>
2014-08-18 21:41:43 -0300

    Add missing licences on file headers

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: cookbook-lamp
+# Recipe:: apache
+#
+# Author:: Juan Manuel Lopez
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Setup athe Apache VirtualHost
 web_app node[:apache][:host_name] do
   template "apache2_sites.conf.erb"
